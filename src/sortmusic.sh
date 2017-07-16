80654f79db028b47c5b411a48eb22e865a2b8523
Nikita <nickita.davidenko@gmail.com>
2017-07-16 16:45:57 +0300

    ident code

--- src/sortmusic.sh
+++ src/sortmusic.sh
@@ -6,24 +6,19 @@ do
   echo "Processing $f file..."
   filename="${f##*/}"
   echo $filename
-mv -- "${f}" "${f%/*}/${filename// /_}"
-		filename=`echo ${filename// /_}`
-
+  mv -- "${f}" "${f%/*}/${filename// /_}"
+  filename=`echo ${filename// /_}`
   artist=`echo $filename |cut -f1 -d "-" | cut -f1 -d "_"`
-echo $artist
-	i="$PWD/$artist"
-		
-
-
+  echo $artist
+  i="$PWD/$artist"
+  
   if ! [ -d "$i" ]
-	then
-       		mkdir $artist
-	     mv $filename $i
-	else
-       	
-         mv $filename $i	 
-  fi
-
+  then
+    mkdir $artist
+    mv $filename $i
+  else
+    mv $filename $i	 
+  fi  
   
 done
 read
